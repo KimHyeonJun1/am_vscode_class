0d7570e42db5681ffe1e78f2dcb7744b7c5513cb
KimHyeonJun1 <167170882+KimHyeonJun1@users.noreply.github.com>
2024-05-29 13:05:31 +0900

    조건문 실습,연습
    북튜토리얼에 폴더를 만들었어야 했는데 프린트튜토리얼에 만들었음(실수) var보단 let 사용하기

---
+++ print_tutorial/05emcc/240529/_datatype.js
@@ -0,0 +1,24 @@
+//14-2 자료형 이해하기
+//자바 자료형 : byte, char, int, Float, Double, String ...
+//Primitibe type :number( = Float, Double), string, boolean,
+// Complex Type : object(array, function) + @ (class, symbol)
+//null, undefined : 특수 자료형
+
+let var1;
+console.log(var1);
+
+//숫자형 (number), 자바에서는 Float, Double 처럼~
+//자바스크립트에서도 BigInt 타입이 있긴한데 실제로 사용x
+
+let score = 100;
+let name = "홍길동"; //이름 100; // 점수
+let isDone = false; //완료여부
+let season = ["봄", "여름", "가을", "겨울"];
+
+console.log(typeof 변수명);
+
+console.log(10 >= 10);
+
+
+
+
